f5a14ba8730d1db5f9fa06b4f46d49d38582a359
Nao YONASHIRO <owan.orisano@gmail.com>
2018-12-28 17:31:59 +0900

    refactor: split filesystem operation

--- main.go
+++ main.go
@@ -13,31 +13,31 @@ import (
 	"github.com/pkg/errors"
 )
 
-type treePrinter struct {
-	w io.Writer
-	readDir func(dirname string) ([]os.FileInfo, error)
+type Node interface {
+	Name() string
+	Children() ([]Node, error)
 }
 
-func (p *treePrinter) Print(dirname string) error {
-	fmt.Fprintln(p.w, dirname)
-	if err := p.printDir(dirname, ""); err != nil {
+type treePrinter struct {
+	w io.Writer
+}
+
+func (p *treePrinter) Print(node Node) error {
+	fmt.Fprintln(p.w, node.Name())
+	if err := p.printChildren(node, ""); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (p *treePrinter) printDir(dirname, prefix string) error {
-	fileInfos, err := p.readDir(dirname)
+func (p *treePrinter) printChildren(node Node, prefix string) error {
+	children, err := node.Children()
 	if err != nil {
-		return errors.Wrapf(err, "failed to read dir %v", dirname)
+		return errors.Wrapf(err, "failed to get children(node=%+v)", node)
 	}
 
-	size := len(fileInfos)
-	for i, fi := range fileInfos {
-		name := fi.Name()
-		if strings.HasPrefix(name, ".") {
-			continue
-		}
+	size := len(children)
+	for i, c := range children {
 		hasNext := i != size-1
 
 		pp := prefix
@@ -48,18 +48,55 @@ func (p *treePrinter) printDir(dirname, prefix string) error {
 			fmt.Fprint(p.w, prefix, "└─── ")
 			pp += "     "
 		}
-		fmt.Fprintln(p.w, name)
-		if fi.IsDir() {
-			if err := p.printDir(filepath.Join(dirname, name), pp); err != nil {
-				return err
-			}
+
+		fmt.Fprintln(p.w, c.Name())
+		if err := p.printChildren(c, pp); err != nil {
+			return err
 		}
 	}
 	return nil
 }
 
-func PrintTree(w io.Writer, dirname string) error {
-	return (&treePrinter{w: w, readDir: ioutil.ReadDir}).Print(dirname)
+type FileSystemNode struct {
+	path  string
+	name  string
+	isDir bool
+}
+
+func (n *FileSystemNode) Name() string {
+	return n.name
+}
+
+func (n *FileSystemNode) Children() ([]Node, error) {
+	if !n.isDir {
+		return nil, nil
+	}
+
+	entries, err := ioutil.ReadDir(n.path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to read dir(path=%v)", n.path)
+	}
+
+	children := make([]Node, 0, len(entries))
+	for _, entry := range entries {
+		name := entry.Name()
+		if strings.HasPrefix(name, ".") {
+			continue
+		}
+		children = append(children, &FileSystemNode{path: filepath.Join(n.path, name), name: name, isDir: entry.IsDir()})
+	}
+	return children, nil
+}
+
+func PrintDirTree(w io.Writer, dirname string) error {
+	fi, err := os.Stat(dirname)
+	if err != nil {
+		return err
+	}
+	if !fi.IsDir() {
+		return errors.Errorf("is not a directory (path=%v)", dirname)
+	}
+	return (&treePrinter{w: w}).Print(&FileSystemNode{path: dirname, name: dirname, isDir: true})
 }
 
 func main() {
@@ -69,7 +106,7 @@ func main() {
 	flag.Parse()
 
 	dir := flag.Arg(0)
-	if err := PrintTree(os.Stdout, dir); err != nil {
+	if err := PrintDirTree(os.Stdout, dir); err != nil {
 		log.Fatal(err)
 	}
 }
